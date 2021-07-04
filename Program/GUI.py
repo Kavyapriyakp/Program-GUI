b15d421604073e9019fa81f066a0384bc742498a
Muneeswari_S_91 <66647070+MuneeswariS1991@users.noreply.github.com>
2021-07-04 20:23:49 +0530

    Update GUI.py

--- Program/GUI.py
+++ Program/GUI.py
@@ -35,11 +35,6 @@ def edgedetect():
     cap = cv2.VideoCapture(0) 
     while(1): 
         ret, frame = cap.read() 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  
-        lower_red = np.array([30,150,50]) 
-        upper_red = np.array([255,255,180]) 
-        mask = cv2.inRange(hsv, lower_red, upper_red) 
-        res = cv2.bitwise_and(frame,frame, mask= mask) 
         edges = cv2.Canny(frame,100,200) 
         cv2.imshow('Input',frame) 
         cv2.imshow('Edges',edges)  
